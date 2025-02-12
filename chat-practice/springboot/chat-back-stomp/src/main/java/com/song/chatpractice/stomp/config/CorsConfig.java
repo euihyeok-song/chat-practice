e4f78ba1377a1c359e7f9d98b50900cf726841a9
EuiHyeok <dmlgur0517@daum.net>
2025-02-13 01:04:07 +0900

    fix: Stomp 전체 파일 위치 변경 ( 구현은 완료 )

--- chat-practice/springboot/chat-back-stomp/src/main/java/com/song/chatpractice/stomp/config/CorsConfig.java
+++ chat-practice/springboot/chat-back-stomp/src/main/java/com/song/chatpractice/stomp/config/CorsConfig.java
@@ -13,6 +13,5 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedOrigins("http://localhost:5173")               // 모든 도메인에서의 접근 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP메소드 (+OPTIONS는 프리플라이트 요청에 사용된다.)
                 .allowedHeaders("*");               // 모든 HTTP 헤더 허용
-//                .allowCredentials(false);           // 인증 정보 허용한 요 허용
     }
 }
